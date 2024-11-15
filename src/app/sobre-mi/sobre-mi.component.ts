ef819ffbd6c7c036f2337527942a4059d6fa4feb
Dayron <dayronrestrepo0212@gmail.com>
2024-11-15 01:07:06 -0500

    Componente contacto

--- src/app/sobre-mi/sobre-mi.component.ts
+++ src/app/sobre-mi/sobre-mi.component.ts
@@ -1,15 +1,25 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-sobre-mi',
   templateUrl: './sobre-mi.component.html',
-  styleUrl: './sobre-mi.component.css',
-  encapsulation: ViewEncapsulation.None  // Deshabilita el encapsulamiento
+  styleUrls: ['./sobre-mi.component.css']
 })
-export class SobreMiComponent {
-  ngOnInit() {
-    AOS.init();
+export class SobreMiComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      // Inicializa AOS después de un breve retraso
+      AOS.init({
+        offset: 100, // Ajusta la activación de las animaciones
+        duration: 1000,
+        once: true
+      });
+    }, 100);
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
